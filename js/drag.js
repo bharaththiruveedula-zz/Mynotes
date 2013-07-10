6cf211d353709d591d7f4886dff0820a4dcec777
Bharath Thiruveedula <bharath_ves@hotmail.com>
2013-07-10 18:42:47 +0530

    Added unique number data attribute to notes

--- js/drag.js
+++ js/drag.js
@@ -2,12 +2,37 @@ $(function() {
 	check();
 	$('.draggable').draggable();
 	$('.draggable').resizable({minHeight:150,minWidth:150});
-	var newnote = '<div class="draggable" class="ui-widget-content"><div class="title">Title</div><div class="content"></div></div>';
+	var newnote = '<div class="draggable" data-rnum class="ui-widget-content"><div class="title">Title</div><div class="content"></div></div>';
 	$('#newnote').on('click', function(){
-		$('#drop').append(newnote);
 		addEvent();
-		localStorage.setItem("notes",localStorage.getItem("notes")+newnote+",");
+		var set = 0;
+		var randomnumber = 0;
+		while(set === 0) {
+			var randomnumber = Math.round(Math.random()*10000);
+			var checkRN = ran(randomnumber);
+			if(checkRN == 1) {
+				set = 1;
+				console.log(randomnumber);
+				var nn =  newnote.replace("data-rnum","data-rnum="+randomnumber);
+				localStorage.setItem("notes", localStorage.getItem("notes")+randomnumber+",");
+				localStorage.setItem(randomnumber, nn); 
+				$('#drop').append(nn);
+			}
+			else
+				continue;
+		}
 	});
+
+	function ran(r) {
+		var nums = localStorage.getItem("notes").split(",");
+		for(var i=0;i<r.length;i++) {
+			if (r === parseInt(nums[i])) 
+				return 0;
+			else
+				continue;
+		}
+		return 1;	
+	}
 
 	function addEvent() {
 		$(".draggable:not(.ui-draggable)").draggable();
@@ -42,8 +67,7 @@ $(function() {
     	var div = $(this);
     	var divHtml = $(this).html();
     	var width = div.width();
-    	console.log(width);
-    	var editableText = $("<input type='text' />").css('width', width);
+    	var editableText = $("<input type='text' data-rnum="+div.parent().data('rnum')+"/>").css('width', width);
     	editableText.val(divHtml);
     	$(this).replaceWith(editableText);
     	editableText.focus();
